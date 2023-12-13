0d3247d4f9989ba0d66bbcdad41e77bbbd486e2f
EvanMworia <evannji99@gmail.com>
2023-12-13 16:49:02 +0300

    Added Models

---
+++ Products_Order_Management/Products_Order_Management/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Products_Order_Management.Models
+{
+    public class Orders
+    {
+        public Guid Id { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Products prods { get; set; } = default!;
+
+        public Guid ProductId { get; set; }
+
+    }
+}
---
+++ Products_Order_Management/Products_Order_Management/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿namespace Products_Order_Management.Models
+{
+    public class Products
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+        //To check if stock of product is available, if not no orders should be accepted
+        public int StockCount { get; set; }
+
+        public List<Orders> OrderList { get; set; }= new List<Orders>();
+
+    }
+}
